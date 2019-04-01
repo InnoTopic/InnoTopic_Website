d0a7f382030a5d6247567eec7c00fb7276674608
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2019-04-01 19:35:52 +0200

    Rotated skill icons, remove skill arrow, remove grayscale on logos

--- InnoTopicWebsite/src/app/skills-shared/skill-level-label/skill-level-label.component.ts
+++ InnoTopicWebsite/src/app/skills-shared/skill-level-label/skill-level-label.component.ts
@@ -1,35 +1,42 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
 import { TopicInterest } from '../../topics-shared/topic-tag/topic-tag.component';
 import { UserSkillLevelsHaveWant } from '../../user-profile-shared/user-skills.service';
 
 @Component({
   selector: 'app-skill-level-label',
   templateUrl: './skill-level-label.component.html',
-  styleUrls: ['./skill-level-label.component.scss']
+  styleUrls: ['./skill-level-label.component.sass'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SkillLevelLabelComponent implements OnInit {
 
   @Input() topic: TopicInterest;
   @Input() skillLevels: UserSkillLevelsHaveWant;
-  icon:string;
+  icon: string;
   // text:string = "set";
-  public isTextVisible:boolean = true;
+  public isTextVisible: boolean = true;
+
 
   public skillsIcons = {
-    none:"battery-empty",
-    beginner:"battery-quarter",
-    intermediate:"battery-half",
-    advanced:"battery-three-quarters",
-    expert:"battery-full"
+    none: 'battery-empty',
+    beginner: "battery-quarter",
+    intermediate: "battery-half",
+    advanced: "battery-three-quarters",
+    expert: "battery-full"
   }
 
   constructor() {
     /// if(topic in userSkills){
       // text = level;
       // icon = skillsIcons.whatever;
-    ///} else {
+    /// } else {
       this.icon = this.skillsIcons.none;
-      //}
+      // }
 
   }
 
