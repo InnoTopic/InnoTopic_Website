f9a058b26e95715c82c4fbc03f76b8359d6a36fc
Karol Depka <karol-depka@users.noreply.github.com>
2021-01-22 02:36:29 +0100

    Remove ngrx and ngxs (problem with rxjs dep in angular upgrade)

--- InnoTopicWebsite/src/store/auth/auth.actions.ts
+++
@@ -1,7 +0,0 @@
-import { AuthenticationStateModel } from './auth.state';
-
-export class SetAuthData {
-  public static readonly type = '[Auth] Auth data';
-
-  constructor(public payload: AuthenticationStateModel) {}
-}
--- InnoTopicWebsite/src/store/dashboard/states/user/user.actions.ts
+++
@@ -1,6 +0,0 @@
-import { PersonStateModel } from './user.state';
-
-export class SetUser {
-  public static readonly type = '[SetUser] action';
-  constructor(public payload: PersonStateModel) {}
-}
