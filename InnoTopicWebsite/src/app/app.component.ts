82704a5e40294afe0f1dba3e076d90f7a2ef2c88
Alan Muñoz <alanmunozparry@gmail.com>
2022-06-10 17:39:07 +0200

    añadido script para detectar preferencias dark/light del usuario

--- InnoTopicWebsite/src/app/app.component.ts
+++ InnoTopicWebsite/src/app/app.component.ts
@@ -22,6 +22,12 @@ export class AppComponent implements OnInit {
       const bodyTag = document.body;
       bodyTag.classList.toggle('dark-theme');
     });
+
+    // DETECTAR SI EL USUARIO PREFIERE DARK MODE
+    // LO SUYO SERÍA SERVIRLE DIRECTAMENTE EL WEBSITE EN LIGHT/DARK SEGÚN TENGA POR DEFECTO EN SU SO / NAVEGADOR
+    const prefersDarkMode = window.matchMedia("(prefers-color-scheme:dark)").matches; // true
+    console.log("prefiere dark mode")
+
   }
 
 
