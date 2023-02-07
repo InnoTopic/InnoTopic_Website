ade72f2538bc5f6704e1e936e5af6d45e96e2788
Karol Depka <karol-depka@users.noreply.github.com>
2023-02-07 01:07:23 +0100

    fix mat imports

--- InnoTopicWebsite/src/app/shared/shared.module.ts
+++ InnoTopicWebsite/src/app/shared/shared.module.ts
@@ -118,11 +118,9 @@
 
 
 import { NgModule } from '@angular/core';
-import {
-  MatButtonModule,
-  MatChipsModule,
-  MatToolbarModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from '../app-routing.module';
