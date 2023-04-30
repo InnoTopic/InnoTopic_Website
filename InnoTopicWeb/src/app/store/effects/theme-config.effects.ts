fafebffc5e8d09ab10038e8943e91092c37b1f84
Karol Depka <karol-depka@users.noreply.github.com>
2023-04-30 02:08:25 +0200

    Auto-set fg color for contrast with bg

--- InnoTopicWeb/src/app/store/effects/theme-config.effects.ts
+++ InnoTopicWeb/src/app/store/effects/theme-config.effects.ts
@@ -4,6 +4,35 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { tap } from 'rxjs/operators';
 import {updateThemeConfig} from "../actions/theme-config-actions";
 
+function getIonicTextColor(backgroundColor: string, contrastValue: 'high' | 'medium' | 'low'): string {
+  // Convert the background color to RGB format
+  const hexToRgb = (hex: string): number[] => {
+    const match = hex.match(/^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i);
+    if (!match) {
+      throw new Error(`Invalid hexadecimal color code: ${hex}`);
+    }
+    const [_, r, g, b] = match;
+    return [parseInt(r, 16), parseInt(g, 16), parseInt(b, 16)];
+  };
+  const rgbBackground: number[] = hexToRgb(backgroundColor);
+
+  // Calculate the relative luminance of the background color
+  const luminance: number = (rgbBackground[0] * 0.299 + rgbBackground[1] * 0.587 + rgbBackground[2] * 0.114) / 255;
+
+  // Determine the appropriate foreground color based on the contrast value
+  if (contrastValue === 'high') {
+    return luminance > 0.5 ? '#000000' : '#ffffff';
+  } else if (contrastValue === 'medium') {
+    return luminance > 0.4 ? '#000000' : '#ffffff';
+  } else if (contrastValue === 'low') {
+    return luminance > 0.6 ? '#000000' : '#ffffff';
+  } else {
+    return '#000000'; // default to black if contrast value is invalid
+  }
+}
+
+
+
 @Injectable()
 export class ThemeConfigEffects {
   constructor(private actions$: Actions) {}
@@ -23,6 +52,12 @@ export class ThemeConfigEffects {
             //--shadow-light-color: #f0f0f0;
             root.setProperty('--shadow-light-color', `#ffffff${action.shadow_opacity}`)
             root.setProperty('--shadow-dark-color' , `#000000${action.shadow_opacity}`)
+            let backgroundColor = action.ion_background_color;
+            if ( backgroundColor ) {
+              let fg = getIonicTextColor(backgroundColor, 'high');
+              root.setProperty('--ion-text-color' , fg)
+              root.setProperty('--color' , fg)
+            }
             //--shadow-dark-color: #d0d0d0;
 
           }
--- InnoTopicWeb/src/app/store/reducers/theme-config-reducer.ts
+++ InnoTopicWeb/src/app/store/reducers/theme-config-reducer.ts
@@ -6,11 +6,11 @@ export const initialState: ThemeConfigState = {
   ion_color_primary: '#007bff',
   ion_color_secondary: '#6c757d',
   // ion_background_color: '#f8f9fa',
-  ion_background_color: '#e0e0e0', //'#ff8080',
-  shadow_offset: '5px',
+  ion_background_color: '#ff3434', //'#ff8080',
+  shadow_offset: '5',
   // shadow_offset_x: '5px',
   // shadow_offset_y: '5px',
-  shadow_blur_radius: '10px',
+  shadow_blur_radius: '10',
   shadow_opacity: '50',
 };
 
